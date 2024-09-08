2d3c9f639652d06d77fdc90340df5d91a969de5d
Ashu Kaushik <ashukaushik1007@gmail.com>
2024-09-08 21:03:07 +0530

    Uploaded

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Header() {
   return (
     <div>
-      <h1 className="text-4xl font-bold text-center mt-4">Header</h1>
+      <h1 className="text-4xl text-center mt-4">Header</h1>
     </div>
   );
 }
