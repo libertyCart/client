367e358dcb29f4078f3138f38131b5a724be7803
Ashu Kaushik <ashukaushik1007@gmail.com>
2024-09-09 14:49:08 +0530

    Header Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import "./App.css";
-import Header from "./components/Header";
+import MyHeader from "./components/MyHeader";
 
 function App() {
   return (
     <>
-      <Header />
+      <MyHeader />
     </>
   );
 }
