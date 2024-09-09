bf82afe49a05afe3c729e8644fbcd9740422949f
Ashu Kaushik <ashukaushik1007@gmail.com>
2024-09-09 22:39:21 +0530

    Banner & Slide  Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import Slider from "./components/Banner/Slider";
 import MyHeader from "./components/MyHeader";
 
 function App() {
   return (
     <>
       <MyHeader />
+      <Slider/>
     </>
   );
 }
