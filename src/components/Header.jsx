27fae73660def8a8c3d8379299adc032245e537e
Ashu Kaushik <ashukaushik1007@gmail.com>
2024-09-13 12:46:53 +0530

    Liberty Card UI Uploaded

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold text-center mt-4">Header</h1>
+    </div>
+  );
+}
+
+export default Header;
